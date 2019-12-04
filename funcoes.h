5c2804b701c19983f2a1dc009ae64dc9b6d6cb5c
mateus-coder <57800865+mateus-coder@users.noreply.github.com>
2019-12-04 15:58:59 -0300

    Update funcoes.h

--- funcoes.h
+++ funcoes.h
@@ -1,19 +1,19 @@
 #ifndef FUNCOES_H_INCLUDED
 #define FUNCOES_H_INCLUDED
 
-int funcaoCadastrarCliente();
+int funcaoCadastrarCliente();///retorna 1 se deu para cadastrar e zero se não
 void imprimiArquivoClientes();
 void imprimiArquivoEstadias();
 void imprimiArquivoQuartos();
 void imprimiArquivoFuncionarios();
-int funcaoCadastrarFuncionarios();
-int funcaoCadastrarQuarto();
-int funcaoCadastrarEstadia(char*,int, char*, char*);
-int funcaoValidarData(char*);
-int funcaoVerificaExistenciaDoCliente(char*);
-int funcaoVerificaExistenciaDeQuarto(int);
-int gerarCodigoEstadia();
+int funcaoCadastrarFuncionarios();///retorna 1 se deu para cadastrar e zero se não
+int funcaoCadastrarQuarto();///retorna 1 se deu para cadastrar e zero se não
+int funcaoCadastrarEstadia(char*,int, char*, char*);///retorna 1 se deu para cadastrar e zero se não
+int funcaoValidarData(char*);///retorna 1 se deu para cadastrar e zero se não
+int funcaoVerificaExistenciaDoCliente(char*);///retorna 1 se deu para cadastrar e zero se não
+int funcaoVerificaExistenciaDeQuarto(int);///retorna 1 se deu para cadastrar e zero se não
+int gerarCodigoEstadia();///retorna 1 se deu para cadastrar e zero se não
 void funcaoMostrarTodasEstadiasDeUmCliente(char*);
-int funcaoBaixaEmUmaEstadia(int);
+int funcaoBaixaEmUmaEstadia(int);///retorna 1 se deu para cadastrar e zero se não
 
 #endif // FUNCOES_H_INCLUDED
