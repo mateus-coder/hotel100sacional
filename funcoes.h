0748fa67e1f8f00bf2bba55a516aa94e757f47c8
mateus-coder <57800865+mateus-coder@users.noreply.github.com>
2019-12-04 23:30:49 -0300

    Update funcoes.h

--- funcoes.h
+++ funcoes.h
@@ -2,10 +2,10 @@
 #define FUNCOES_H_INCLUDED
 
 int funcaoCadastrarCliente();///retorna 1 se deu para cadastrar e zero se não
-void imprimiArquivoClientes();
+void imprimiArquivoClientes(char*);
 void imprimiArquivoEstadias();
 void imprimiArquivoQuartos();
-void imprimiArquivoFuncionarios();
+void imprimiArquivoFuncionarios(char*);
 int funcaoCadastrarFuncionarios();///retorna 1 se deu para cadastrar e zero se não
 int funcaoCadastrarQuarto();///retorna 1 se deu para cadastrar e zero se não
 int funcaoCadastrarEstadia(char*,int, char*, char*);///retorna 1 se deu para cadastrar e zero se não
@@ -14,6 +14,6 @@ int funcaoVerificaExistenciaDoCliente(char*);///retorna 1 se deu para cadastrar 
 int funcaoVerificaExistenciaDeQuarto(int);///retorna 1 se deu para cadastrar e zero se não
 int gerarCodigoEstadia();///retorna 1 se deu para cadastrar e zero se não
 void funcaoMostrarTodasEstadiasDeUmCliente(char*);
-int funcaoBaixaEmUmaEstadia(int);///retorna 1 se deu para cadastrar e zero se não
-
+int funcaoBaixaEmUmaEstadia(int, float*);///retorna 1 se deu para cadastrar e zero se não
+int funcaoPontosDeFidelidade(char*); ///retorna 0 se não for possível ler o arquivo ou se não tiver o nome procurado cadastrado e retorna a soma dos pontos de fidelidade se o cliente estiver cadastrado e estiver dado baixa em alguma estadia;
 #endif // FUNCOES_H_INCLUDED
